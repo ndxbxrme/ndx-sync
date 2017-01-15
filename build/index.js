a3f6dcc2cd2f65727a3000152d80532c80edcd6c
ndxbxrme <lewis_the_cat@hotmail.com>
2017-01-15 10:32:21 +0000

    first commit

---
+++ build/index.js
@@ -0,0 +1,117 @@
+(function() {
+  'use strict';
+  var async;
+
+  async = require('async');
+
+  module.exports = function(ndx) {
+    var getServersToNotify, io, parentId, serverSockets, socket;
+    console.log('sync', ndx.host);
+    serverSockets = [];
+    socket = null;
+    parentId = '';
+    getServersToNotify = function(type, args) {
+      var i, len, output, server;
+      output = [];
+      if (!args.recipients) {
+        args.recipients = ndx.id + ',';
+        if (parentId) {
+          args.recipients += parentId + ',';
+          socket.emit(type, args);
+        }
+      }
+      for (i = 0, len = serverSockets.length; i < len; i++) {
+        server = serverSockets[i];
+        if (args.recipients.indexOf(server.serverId) === -1) {
+          output.push(server);
+          args.recipients += server.serverId + ',';
+        }
+      }
+      return output;
+    };
+    if (ndx.host) {
+      ndx.socket.on('connection', function(socket) {
+        socket.on('server', function(data) {
+          console.log('got server', data.id, ndx.id);
+          if (data.id === ndx.id) {
+            console.log('dropping server');
+            return socket.disconnect();
+          } else {
+            socket.serverId = data.id;
+            socket.emit('server', {
+              id: ndx.id
+            });
+            return serverSockets.push(socket);
+          }
+        });
+        socket.on('update', function(args) {
+          return ndx.database.serverExec('update', args);
+        });
+        socket.on('insert', function(args) {
+          return ndx.database.serverExec('insert', args);
+        });
+        return socket.on('delete', function(args) {
+          return ndx.database.serverExec('delete', args);
+        });
+      });
+      ndx.socket.on('disconnect', function(socket) {
+        if (socket.serverId) {
+          return serverSockets.splice(serverSockets.indexOf(socket), 1);
+        }
+      });
+      ndx.database.on('update', function(args) {
+        var servers;
+        servers = getServersToNotify('update', args);
+        return async.each(servers, function(server, callback) {
+          server.emit('update', args);
+          return callback();
+        });
+      });
+      ndx.database.on('insert', function(args) {
+        var servers;
+        servers = getServersToNotify('insert', args);
+        return async.each(servers, function(server, callback) {
+          server.emit('insert', args);
+          return callback();
+        });
+      });
+      ndx.database.on('delete', function(args) {
+        var servers;
+        servers = getServersToNotify('delete', args);
+        return async.each(servers, function(server, callback) {
+          server.emit('delete', args);
+          return callback();
+        });
+      });
+      io = require('socket.io-client');
+      socket = io.connect('http://' + ndx.host, {
+        reconnect: true
+      });
+      socket.on('connect', function() {
+        console.log('client connected');
+        socket.emit('server', {
+          id: ndx.id
+        });
+        socket.on('server', function(args) {
+          console.log('parent server', args.id);
+          return parentId = args.id;
+        });
+        socket.on('update', function(args) {
+          return ndx.database.serverExec('update', args);
+        });
+        socket.on('insert', function(args) {
+          return ndx.database.serverExec('insert', args);
+        });
+        return socket.on('delete', function(args) {
+          return ndx.database.serverExec('delete', args);
+        });
+      });
+      return socket.on('disconnect', function() {
+        return console.log('client disconnected');
+      });
+    }
+  };
+
+}).call(this);
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.coffee
@@ -0,0 +1,73 @@
+'use strict'
+
+async = require 'async'
+
+module.exports = (ndx) ->
+  console.log 'sync', ndx.host
+  serverSockets = []
+  socket = null
+  parentId = ''
+  getServersToNotify = (type, args) ->
+    output = []
+    if not args.recipients
+      args.recipients = ndx.id + ','
+      if parentId
+        args.recipients += parentId + ','
+        socket.emit type, args
+    for server in serverSockets
+      if args.recipients.indexOf(server.serverId) is -1
+        output.push server
+        args.recipients += server.serverId + ','
+    output
+  if ndx.host
+    ndx.socket.on 'connection', (socket) ->
+      socket.on 'server', (data) ->
+        console.log 'got server', data.id, ndx.id
+        if data.id is ndx.id
+          console.log 'dropping server'
+          socket.disconnect()
+        else
+          socket.serverId = data.id
+          socket.emit 'server', id: ndx.id
+          serverSockets.push socket
+      socket.on 'update', (args) ->
+        ndx.database.serverExec 'update', args
+      socket.on 'insert', (args) ->
+        ndx.database.serverExec 'insert', args
+      socket.on 'delete', (args) ->
+        ndx.database.serverExec 'delete', args
+    ndx.socket.on 'disconnect', (socket) ->
+      if socket.serverId
+        serverSockets.splice serverSockets.indexOf(socket), 1
+    ndx.database.on 'update', (args) ->
+      servers = getServersToNotify 'update', args
+      async.each servers, (server, callback) ->
+        server.emit 'update', args
+        callback()
+    ndx.database.on 'insert', (args) ->
+      servers = getServersToNotify 'insert', args
+      async.each servers, (server, callback) ->
+        server.emit 'insert', args
+        callback()
+    ndx.database.on 'delete', (args) ->
+      servers = getServersToNotify 'delete', args
+      async.each servers, (server, callback) ->
+        server.emit 'delete', args
+        callback()
+    io = require 'socket.io-client'
+    socket = io.connect 'http://' + ndx.host, reconnect: true
+    socket.on 'connect', ->
+      console.log 'client connected'
+      socket.emit 'server', id:ndx.id
+      socket.on 'server', (args) ->
+        console.log 'parent server', args.id
+        parentId = args.id
+      socket.on 'update', (args) ->
+        ndx.database.serverExec 'update', args
+      socket.on 'insert', (args) ->
+        ndx.database.serverExec 'insert', args
+      socket.on 'delete', (args) ->
+        ndx.database.serverExec 'delete', args
+    socket.on 'disconnect', ->
+      console.log 'client disconnected'
+      
